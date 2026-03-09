e68032707e5436dfe5cbbbc64118094cbff212db
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-03-09 16:58:36 +0300

    Update Task2.cs

--- Lab7/White/Task2.cs
+++ Lab7/White/Task2.cs
@@ -1,6 +1,42 @@
-﻿namespace Lab7.White
+using System;
+
+public struct Participant
 {
-    public class Task2
+    private string name;
+    private string surname;
+    private double firstJump;
+    private double secondJump;
+
+    public string Name => name;
+    public string Surname => surname;
+    public double FirstJump => firstJump;
+    public double SecondJump => secondJump;
+
+    public double BestJump => Math.Max(firstJump, secondJump);
+
+    public Participant(string name, string surname)
     {
+        this.name = name;
+        this.surname = surname;
+        firstJump = 0;
+        secondJump = 0;
+    }
+
+    public void Jump(double result)
+    {
+        if (firstJump == 0)
+            firstJump = result;
+        else if (secondJump == 0)
+            secondJump = result;
+    }
+
+    public static void Sort(Participant[] array)
+    {
+        Array.Sort(array, (a, b) => b.BestJump.CompareTo(a.BestJump));
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{name} {surname} | {firstJump} | {secondJump} | Best: {BestJump}");
     }
 }
